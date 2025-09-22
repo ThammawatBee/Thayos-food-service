d252a2f9b4b89ee47fd575295d30ef3a46322aaa
Bee <thammawat@muze.co.th>
2025-09-22 20:59:36 +0700

    update delivery report

--- src/utils/bag.ts
+++ src/utils/bag.ts
@@ -1,6 +1,7 @@
 import { NotFoundException } from '@nestjs/common';
 import { DateTime } from 'luxon';
 import { Bag } from 'src/entities/bag.entity';
+import { OrderItem } from 'src/entities/orderItem.entity';
 
 export const getDeliveryGroup = (date: DateTime): string => {
   const day = date.weekday; // 1 = Mon ... 7 = Sun
@@ -67,4 +68,24 @@ export const modifyGroupBag = (bags: Bag[]) => {
       order: bags[0].order,
     };
   }
+};
+
+export const modifyDeliverBag = (bags: Bag[]) => {
+  let allOrderItems: OrderItem[] = [];
+  bags.forEach((bag) => {
+    allOrderItems = [...allOrderItems, ...bag.orderItems];
+  });
+  return {
+    deliveryAt: `${bags[0].deliveryAt}`,
+    customerName: bags[0].order.customer.fullname,
+    address: bags[0].order.address,
+    customerCode: bags[0].order.customer.customerCode,
+    orderItems: allOrderItems,
+    noRemarkType: bags[0].noRemarkType,
+    remark: bags[0].order.customer.remark,
+    qrCode: bags[0].qrCode,
+    inBasketStatus: bags[0].inBasketStatus,
+    basket: bags[0].basket,
+    order: bags[0].order,
+  };
 };
